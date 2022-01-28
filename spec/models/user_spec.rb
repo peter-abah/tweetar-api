30b5d46fb8c1bf7cc61bec8df2b5daa2db435a10
Peter Abah <peterabah.ob@gmail.com>
2022-01-28 19:52:29 +0100

    Add validation of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,4 +30,6 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of(:password) }
   it { should validate_length_of(:password).is_at_least(8) }
+
+  it { should validate_presence_of(:email) }
 end
