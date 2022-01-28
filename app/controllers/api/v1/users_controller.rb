09949513b3068acc7a23b1ab97914ca30224fe98
Peter Abah <peterabah.ob@gmail.com>
2022-01-28 18:51:11 +0100

    Add users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,21 @@
-class UsersController < ApplicationController
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        user = User.create(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          p user
+          render json: { error: user.errors.full_messages.first }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :password, :first_name, :last_name)
+      end
+    end
+  end
 end
