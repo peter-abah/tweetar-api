1a867fe6aa163952441b079d7103d8ca7a051277
Peter Abah <peterabah.ob@gmail.com>
2022-01-28 18:51:44 +0100

    Fix failing tests

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -7,25 +7,28 @@ RSpec.describe 'Authentications', type: :request do
     it 'authenticates the user' do
       post '/api/v1/login', params: { username: user.username, password: 'password' }
       expect(response).to have_http_status(:created)
-      expect(json).to eq({
-                           'id' => user.id,
-                           'username' => 'user1',
-                           'token' => AuthenticationTokenService.call(user.id)
-                         })
+      expect(JSON.parse(response.body)).to eq({
+                                                'id' => user.id,
+                                                'username' => 'user1',
+                                                'first_name' => user.first_name,
+                                                'last_name' => user.last_name,
+                                                'bio' => nil,
+                                                'token' => AuthenticationTokenService.call(user.id)
+                                              })
     end
     it 'returns error when username does not exist' do
       post '/api/v1/login', params: { username: 'ac', password: 'password' }
       expect(response).to have_http_status(:unauthorized)
-      expect(json).to eq({
-                           'error' => 'No such user'
-                         })
+      expect(JSON.parse(response.body)).to eq({
+                                                'error' => 'No such user'
+                                              })
     end
     it 'returns error when password is incorrect' do
-      post '/api/v1/login', params: { username: user.username, password: 'incorrect' }
+      post '/api/v1/login', params: { username: user.username, password: 'incorrect', bio: nil }
       expect(response).to have_http_status(:unauthorized)
-      expect(json).to eq({
-                           'error' => 'Incorrect password'
-                         })
+      expect(JSON.parse(response.body)).to eq({
+                                                'error' => 'Incorrect password'
+                                              })
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'POST /register' do
     it 'authenticates the user' do
-      post '/api/v1/register', params: { user: { username: 'user1', password: 'password' } }
+      post '/api/v1/register',
+           params: { user: { username: 'user1', password: 'password', first_name: 'first', last_name: 'last' } }
       expect(response).to have_http_status(:created)
-      expect(json).to eq({
-                           'id' => User.last.id,
-                           'username' => 'user1',
-                           'token' => AuthenticationTokenService.call(User.last.id)
-                         })
+      expect(JSON.parse(response.body)).to eq({
+                                                'id' => User.last.id,
+                                                'username' => 'user1',
+                                                'token' => AuthenticationTokenService.call(User.last.id),
+                                                'first_name' => 'first',
+                                                'last_name' => 'last',
+                                                'bio' => nil
+                                              })
     end
   end
 end
