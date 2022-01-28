770723896322df18140939c136400f1c24854eef
Peter Abah <peterabah.ob@gmail.com>
2022-01-28 22:14:52 +0100

    Add authentication methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+
+  def payload
+    auth_header = request.headers['Authorization'];
+    token = auth_header.split(' ').last
+    AuthenticationTokenService.decode(token)[0]
+  rescue StandardError
+    nil
+  end
+
+  def current_user!
+    @current_user = User.find_by(id: payload['user_id'])
+  end
+
+  def invalid_authentication
+    render json: { error: 'You will need to login first' }, status: :unauthorized
+  end
+
+  def authenticate_request!
+    return invalid_authentication if !payload || !AuthenticationTokenService.valid_payload(payload.first)
+
+    current_user!
+    invalid_authentication unless @current_user
+  end
 end
