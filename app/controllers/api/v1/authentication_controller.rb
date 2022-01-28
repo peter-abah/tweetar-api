163b21794588ba294f01f24aa8525f6f987f5634
Peter Abah <peterabah.ob@gmail.com>
2022-01-28 18:50:18 +0100

    Add authentication controller for users

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,2 +1,34 @@
-class AuthenticationController < ApplicationController
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError; end
+
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticationError, with: :handle_unauthenticated
+
+      def create
+        if user
+          raise AuthenticationError unless user.authenticate(params.require(:password))
+
+          render json: user, status: :created
+        else
+          render json: { error: 'No such user' }, status: :unauthorized
+        end
+      end
+
+      private
+
+      def user
+        @user ||= User.find_by(username: params.require(:username))
+      end
+
+      def parameter_missing(error)
+        render json: { error: error.message }, status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        render json: { error: 'Incorrect password' }, status: :unauthorized
+      end
+    end
+  end
 end
