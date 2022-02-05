99ad618e9f80c73a279512c40e28ea701740004b
Peter Abah <peterabah.ob@gmail.com>
2022-02-06 00:20:42 +0100

    Remove database cleaner in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require 'database_cleaner-active_record'
 require_relative './seeds/seed_tweets'
 require_relative './seeds/seed_users'
 require_relative './seeds/seed_followers'
