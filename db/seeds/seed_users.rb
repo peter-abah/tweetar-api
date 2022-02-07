a65b98d4eb28ab544f53d0e4a121ae8d8771da91
Peter Abah <peterabah.ob@gmail.com>
2022-02-08 00:39:51 +0100

    Add cover image for seed users

--- db/seeds/seed_users.rb
+++ db/seeds/seed_users.rb
@@ -2,7 +2,8 @@ def seed_users(n = 20)
   0.upto(n) do
     url = Faker::Avatar.image
     filename = File.basename(URI.parse(url).path)
-    file = URI.open(url)
+    profile_img = URI.open(url)
+    cover_img = File.open("#{Rails.root}/app/assets/images/1.jpg")
 
     user = User.create!({
       username: Faker::Internet.unique.username,
@@ -13,6 +14,7 @@ def seed_users(n = 20)
       password: 'password'
     })
 
-    user.profile_image.attach(io: file, filename: filename, content_type: 'image/jpeg')
+    user.profile_image.attach(io: profile_img, filename: filename, content_type: 'image/jpeg')
+    user.cover_image.attach(io: cover_img, filename: 'cover_image', content_type: 'image/jpeg')
   end
 end
