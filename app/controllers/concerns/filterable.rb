0b6a7c4ee037b6d59acbaede3b46bae4259cdac7
Peter Abah <peterabah.ob@gmail.com>
2022-02-14 19:31:32 +0100

    Add filtering of tweets by user

--- app/controllers/concerns/filterable.rb
+++ app/controllers/concerns/filterable.rb
@@ -1,7 +1,16 @@
 # Filters an active record relation if a query is present in params
 module Filterable
   def filter(relation)
+    filter_query filter_user(relation)
+  end
+
+  def filter_query(relation)
     query = params[:q]
     query ? relation.filter_by_query(query) : relation
+  end
+
+  def filter_user(relation)
+    user_id = params[:user_id]
+    user_id ? relation.where(user_id: user_id) : relation
   end
 end
